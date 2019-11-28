d2657e896fbf90c870600351b2a0d9c6761b4265
FFFro <www.839782562@qq.com>
2019-11-28 21:31:24 +0800

    leetcode-20/21-20191128

---
+++ leetcode/TreeNode.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+/**
+ * @program: arithmetic
+ * @description: 二叉树
+ * @author: FFFro
+ * @create: 2019-11-28 21:21
+ **/
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
