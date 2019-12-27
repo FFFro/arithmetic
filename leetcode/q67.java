472086f03a46482f6607ff57e9a2d1f3f3c6dd73
FFFro <www.839782562@qq.com>
2019-12-27 19:47:44 +0800

    leetcode-66/67-2019-12-27

---
+++ leetcode/q67.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+/**
+ * 移动零
+ * @author : FFFro
+ * @date : 2019-12-27 19:41
+ **/
+public class q67 {
+
+    public void moveZeroes(int[] nums) {
+        int curr = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[curr] = nums[i];
+                if (curr != i) {
+                    nums[i] = 0;
+                }
+
+                curr++;
+            }
+        }
+    }
+}
+
+//    给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+//
+//        示例:
+//
+//        输入: [0,1,0,3,12]
+//        输出: [1,3,12,0,0]
+//        说明:
+//
+//        必须在原数组上操作，不能拷贝额外的数组。
+//        尽量减少操作次数。
